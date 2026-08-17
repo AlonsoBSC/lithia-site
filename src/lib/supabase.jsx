92c04a5fa07cb064713876fac5a74a8dfa7692f9
diego <dinostroza@besaconsultores.cl>
2026-08-17 10:57:37 -0400

    fix hardcoded supabase credentials

--- src/lib/supabase.jsx
+++ src/lib/supabase.jsx
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+﻿import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://mnggqrkyysuonnomxuch.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uZ2dxcmt5eXN1b25ub214dWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNTAyNzksImV4cCI6MjA3NzgyNjI3OX0.Xmj09Hj6vxxRlyCOAJQGNZMhcUB56hvOgKAPQ8XDww0';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
